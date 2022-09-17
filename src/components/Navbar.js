b2a3338c5cd178cbf9f9c195c806c6d6cad42ebb
Yogesh1221 <yogesh.singh200001@gmail.com>
2022-09-17 16:21:28 +0530

    fixed the bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
       <div className="container-fluid">
         {/* <a className="navbar-brand" href="">{props.title}</a> */}
-        <Link className="navbar-brand" to="/">{props.title}</Link>
+        <a className="navbar-brand" href="">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -15,12 +15,12 @@ export default function Navbar(props) {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               {/* <a className="nav-link active" aria-current="page" href="">Home</a> */}
-              <Link className="nav-link" aria-current="page" to="/">Home</Link>
+              <a className="nav-link" aria-current="page" href="">Home</a>
 
             </li>
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link className="nav-link" to="/about">{props.about}</Link>
-            </li>
+            </li> */}
           </ul>
           {/* <form className="d-flex" role="search">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
